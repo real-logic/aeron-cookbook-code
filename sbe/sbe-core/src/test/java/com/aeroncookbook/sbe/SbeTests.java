99af1b64cea77b888f55404a3d10fe29d4771e14
adaptive-sl <534336+adaptive-sl@users.noreply.github.com>
2019-11-23 14:36:23 -0500

    First code push

---
+++ sbe/sbe-core/src/test/java/com/aeroncookbook/sbe/SbeTests.java
@@ -0,0 +1,12 @@
+package com.aeroncookbook.sbe;
+
+import org.junit.jupiter.api.Test;
+
+public class SbeTests
+{
+    @Test
+    public void canWriteReadHeader()
+    {
+        final SampleAEncoder f = null;
+    }
+}
