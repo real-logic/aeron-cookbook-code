def4c049a2af5e930c328a5b2ea9866a74027002
Shaun Laurens <shaun@weareadaptive.com>
2019-12-02 11:33:03 -0500

    Simplest case IPC

---
+++ aeron-ipc/README.md
@@ -0,0 +1,5 @@
+#Aeron IPC Sample
+
+Includes one projects:
+
+* `ipc-core` - contains two agents using IPC to communicate
---
+++ aeron-ipc/ipc-core/src/main/java/com/aeroncookbook/ipc/SimplestCase.java
@@ -0,0 +1,52 @@
+package com.aeroncookbook.ipc;
+
+import java.nio.ByteBuffer;
+
+import io.aeron.Aeron;
+import io.aeron.Publication;
+import io.aeron.Subscription;
+import io.aeron.driver.MediaDriver;
+import io.aeron.logbuffer.FragmentHandler;
+import org.agrona.concurrent.IdleStrategy;
+import org.agrona.concurrent.SleepingIdleStrategy;
+import org.agrona.concurrent.UnsafeBuffer;
+
+public class SimplestCase
+{
+    public static void main(String[] args)
+    {
+        final String channel = "aeron:ipc";
+        final String message = "my message";
+
+        final IdleStrategy idle = new SleepingIdleStrategy();
+        final UnsafeBuffer unsafeBuffer = new UnsafeBuffer(ByteBuffer.allocate(256));
+
+        try (MediaDriver driver = MediaDriver.launch();
+             Aeron aeron = Aeron.connect();
+             Subscription sub = aeron.addSubscription(channel, 10);
+             Publication pub = aeron.addPublication(channel, 10))
+        {
+            while (!pub.isConnected())
+            {
+                idle.idle();
+            }
+
+            unsafeBuffer.putStringAscii(0, message);
+            System.out.println("sending:" + message);
+            while (pub.offer(unsafeBuffer) < 0)
+            {
+                idle.idle();
+            }
+
+            FragmentHandler handler = (buffer, offset, length, header) ->
+            {
+                System.out.println("received:" + buffer.getStringAscii(offset));
+            };
+
+            while (sub.poll(handler, 1) <= 0)
+            {
+                idle.idle();
+            }
+        }
+    }
+}
