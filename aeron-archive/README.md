e6eb7769838582abcae3459c9b77430d6303ea1a
adaptive-sl <534336+adaptive-sl@users.noreply.github.com>
2019-12-07 16:15:25 -0500

    Initial archive checkin

---
+++ aeron-archive/README.md
@@ -0,0 +1,6 @@
+#Aeron Archive Sample
+
+`archive-core` - contains two executable items
+
+  - `SimplestCase.java` - a one file Aeron IPC sample.
+  - `com.aeroncookbook.archive.agents.*` - Two processes that shared data with Aeron Archive using Live Replay.
---
+++ aeron-archive/archive-core/src/main/java/com/aeroncookbook/archive/agents/ReceiveAgent.java
@@ -0,0 +1,48 @@
+package com.aeroncookbook.archive.agents;
+
+import io.aeron.Subscription;
+import io.aeron.logbuffer.Header;
+import org.agrona.DirectBuffer;
+import org.agrona.concurrent.Agent;
+import org.agrona.concurrent.ShutdownSignalBarrier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReceiveAgent implements Agent
+{
+    private final Subscription subscription;
+    private final ShutdownSignalBarrier barrier;
+    private final int sendCount;
+    private final Logger logger = LoggerFactory.getLogger(ReceiveAgent.class);
+
+    public ReceiveAgent(final Subscription subscription, ShutdownSignalBarrier barrier, int sendCount)
+    {
+        this.subscription = subscription;
+        this.barrier = barrier;
+        this.sendCount = sendCount;
+    }
+
+    @Override
+    public int doWork() throws Exception
+    {
+        subscription.poll(this::handler, 1);
+        return 0;
+    }
+
+    private void handler(DirectBuffer buffer, int offset, int length, Header header)
+    {
+        final int lastValue = buffer.getInt(offset);
+        logger.info("received: {}", lastValue);
+
+        if (lastValue >= sendCount)
+        {
+            barrier.signal();
+        }
+    }
+
+    @Override
+    public String roleName()
+    {
+        return "receiver";
+    }
+}
