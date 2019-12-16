c239798bb969b24cd7034ad5f149e58b2d4a4977
Shaun Laurens <shaun@weareadaptive.com>
2019-12-16 13:42:33 -0500

    Actual working cluster sample

--- aeron-cluster/cluster-core/src/main/java/com/aeroncookbook/cluster/SimplestCase.java
+++ aeron-cluster/cluster-core/src/main/java/com/aeroncookbook/cluster/SimplestCase.java
@@ -1,54 +1,212 @@
 package com.aeroncookbook.cluster;
 
 import io.aeron.Aeron;
+import io.aeron.CommonContext;
+import io.aeron.Image;
 import io.aeron.Publication;
-import io.aeron.Subscription;
+import io.aeron.archive.Archive;
+import io.aeron.archive.ArchiveThreadingMode;
+import io.aeron.cluster.ClusteredMediaDriver;
+import io.aeron.cluster.ConsensusModule;
+import io.aeron.cluster.client.AeronCluster;
+import io.aeron.cluster.client.EgressListener;
+import io.aeron.cluster.codecs.CloseReason;
+import io.aeron.cluster.service.ClientSession;
+import io.aeron.cluster.service.Cluster;
+import io.aeron.cluster.service.ClusteredService;
+import io.aeron.cluster.service.ClusteredServiceContainer;
 import io.aeron.driver.MediaDriver;
-import io.aeron.logbuffer.FragmentHandler;
-import org.agrona.concurrent.IdleStrategy;
-import org.agrona.concurrent.SleepingIdleStrategy;
-import org.agrona.concurrent.UnsafeBuffer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.nio.ByteBuffer;
+import io.aeron.driver.MinMulticastFlowControlSupplier;
+import io.aeron.driver.ThreadingMode;
+import io.aeron.logbuffer.Header;
+import org.agrona.CloseHelper;
+import org.agrona.DirectBuffer;
+import org.agrona.ExpandableArrayBuffer;
+import org.agrona.console.ContinueBarrier;
 
 public class SimplestCase
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimplestCase.class);
-
-    public static void main(String[] args)
+    static final ExpandableArrayBuffer msgBuffer = new ExpandableArrayBuffer();
+    static final EgressListener egressMessageListener = new EgressListener()
     {
-        final String channel = "aeron:ipc";
-        final String message = "my message";
-
-        final IdleStrategy idle = new SleepingIdleStrategy();
-        final UnsafeBuffer unsafeBuffer = new UnsafeBuffer(ByteBuffer.allocate(256));
-
-        try (MediaDriver driver = MediaDriver.launch();
-             Aeron aeron = Aeron.connect();
-             Subscription sub = aeron.addSubscription(channel, 10);
-             Publication pub = aeron.addPublication(channel, 10))
+        @Override
+        public void onMessage(long clusterSessionId, long timestamp, DirectBuffer buffer, int offset, int length, Header header)
         {
-            while (!pub.isConnected())
-            {
-                idle.idle();
-            }
+            System.out.println("got message from " + clusterSessionId);
+        }
 
-            unsafeBuffer.putStringAscii(0, message);
-            LOGGER.info("sending:{}", message);
-            while (pub.offer(unsafeBuffer) < 0)
-            {
-                idle.idle();
-            }
+        @Override
+        public void newLeader(long clusterSessionId, long leadershipTermId, int leaderMemberId, String memberEndpoints)
+        {
+            System.out.println("new leader");
+        }
+    };
+    static Aeron aeron;
+    static ClusteredMediaDriver clusteredMediaDriver;
+    static ClusteredServiceContainer container;
+    static SimplestCase.Service service;
 
-            FragmentHandler handler = (buffer, offset, length, header) ->
-                    LOGGER.info("received:{}", buffer.getStringAscii(offset));
+    static MediaDriver clientMediaDriver;
+    static AeronCluster client;
 
-            while (sub.poll(handler, 1) <= 0)
-            {
-                idle.idle();
-            }
+    static void start(final boolean deleteOnStart)
+    {
+        final MediaDriver.Context mediaDriverContext = new MediaDriver.Context();
+        final ConsensusModule.Context consensusModuleContext = new ConsensusModule.Context();
+        final Archive.Context archiveContext = new Archive.Context();
+        final ClusteredServiceContainer.Context serviceContainerContext = new ClusteredServiceContainer.Context();
+
+        service = new SimplestCase.Service();
+
+        mediaDriverContext
+                .threadingMode(ThreadingMode.SHARED)
+                .multicastFlowControlSupplier(new MinMulticastFlowControlSupplier())
+                .errorHandler(Throwable::printStackTrace)
+                .dirDeleteOnShutdown(true)
+                .dirDeleteOnStart(true);
+
+        archiveContext
+                .recordingEventsEnabled(true)
+                .threadingMode(ArchiveThreadingMode.SHARED);
+
+        consensusModuleContext
+                .errorHandler(Throwable::printStackTrace)
+                .deleteDirOnStart(deleteOnStart);
+
+        serviceContainerContext
+                .clusteredService(service)
+                .errorHandler(Throwable::printStackTrace);
+
+        clusteredMediaDriver = ClusteredMediaDriver.launch(
+                mediaDriverContext,
+                archiveContext,
+                consensusModuleContext
+        );
+
+        container = ClusteredServiceContainer.launch(serviceContainerContext);
+    }
+
+    static void stop()
+    {
+        CloseHelper.quietClose(client);
+        CloseHelper.quietClose(container);
+        CloseHelper.quietClose(clusteredMediaDriver);
+        CloseHelper.quietClose(clientMediaDriver);
+    }
+
+    static void sendMessage(final int id, int messageLength)
+    {
+        msgBuffer.putInt(0, id);
+        while (client.offer(msgBuffer, 0, messageLength) < 0) {
+            client.pollEgress();
+            Thread.yield();
         }
     }
+
+    static void pollEgress()
+    {
+        while(client.pollEgress() <= 0) {
+            Thread.yield();
+        }
+    }
+
+    public static void main(final String[] args)
+    {
+        start(false);
+        connectClient();
+        sendMessage(1, 4);
+        pollEgress();
+        ContinueBarrier barrier = new ContinueBarrier("continue");
+        barrier.await();
+        stop();
+    }
+
+    static void connectClient()
+    {
+        final String aeronDirName = CommonContext.getAeronDirectoryName() + "-client";
+
+        clientMediaDriver = MediaDriver.launch(
+                new MediaDriver.Context()
+                        .threadingMode(ThreadingMode.SHARED)
+                        .dirDeleteOnStart(true)
+                        .dirDeleteOnStart(true)
+                        .errorHandler(Throwable::printStackTrace)
+                        .aeronDirectoryName(aeronDirName)
+        );
+
+        client = AeronCluster.connect(
+                new AeronCluster.Context()
+                        .errorHandler(Throwable::printStackTrace)
+                        .egressListener(egressMessageListener)
+                        .aeronDirectoryName(aeronDirName)
+        );
+
+    }
+
+    static class Service implements ClusteredService
+    {
+
+        private Cluster cluster;
+
+        @Override
+        public void onStart(Cluster cluster, Image snapshotImage)
+        {
+            this.cluster = cluster;
+        }
+
+        @Override
+        public void onSessionOpen(ClientSession session, long timestamp)
+        {
+            System.out.println("session opened from session " + session.id());
+        }
+
+        @Override
+        public void onSessionClose(ClientSession session, long timestamp, CloseReason closeReason)
+        {
+            System.out.println("session closed id=" + session.id());
+        }
+
+        @Override
+        public void onSessionMessage(ClientSession session, long timestamp, DirectBuffer buffer, int offset, int length, Header header)
+        {
+            System.out.println("session message");
+            while (session.offer(buffer, offset, length) < 0) {
+                Thread.yield();
+            }
+        }
+
+        @Override
+        public void onTimerEvent(long correlationId, long timestamp)
+        {
+            System.out.println("session timer fired");
+
+        }
+
+        @Override
+        public void onTakeSnapshot(Publication snapshotPublication)
+        {
+            System.out.println("session opened");
+
+        }
+
+        @Override
+        public void onRoleChange(Cluster.Role newRole)
+        {
+            System.out.println("role change to " + newRole);
+        }
+
+        @Override
+        public void onTerminate(Cluster cluster)
+        {
+            System.out.println("terminated");
+
+        }
+
+        protected long serviceCorrelationId(final int correlationId)
+        {
+            return ((long) cluster.context().serviceId()) << 32 | correlationId;
+        }
+
+    }
+
 }
