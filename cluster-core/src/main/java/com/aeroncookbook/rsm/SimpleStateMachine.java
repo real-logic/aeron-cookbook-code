49dd5b166445676230cc5fbf3e665b4daf0c70cf
eleventy7 <534336+eleventy7@users.noreply.github.com>
2020-05-17 07:44:27 -0400

    lamport, rsm, gradle 641

---
+++ cluster-core/src/main/java/com/aeroncookbook/rsm/SimpleStateMachine.java
@@ -0,0 +1,67 @@
+/*
+ *
+ *  * Copyright 2019-2020 eleventy7
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.aeroncookbook.rsm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleStateMachine
+{
+    private int currentValue = 0;
+    private List<EventListener> eventListeners = new ArrayList<>();
+
+    public void addListener(EventListener eventListener)
+    {
+        eventListeners.add(eventListener);
+    }
+
+    public void add(AddCommand addCommand)
+    {
+        currentValue += addCommand.value;
+        notifyListeners();
+    }
+
+    public void multiply(MultiplyCommand multiplyCommand)
+    {
+        currentValue *= multiplyCommand.value;
+        notifyListeners();
+    }
+
+    public void set(SetCommand setCommand)
+    {
+        currentValue = setCommand.value;
+        notifyListeners();
+    }
+
+    public void snapshot(SnapshotCommand snapshotCommand)
+    {
+        notifyListeners();
+    }
+
+    private void notifyListeners()
+    {
+        final NewValueEvent newValueEvent = new NewValueEvent();
+        newValueEvent.currentValue = currentValue;
+        for (final EventListener eventListener : eventListeners)
+        {
+            eventListener.newValue(newValueEvent);
+        }
+    }
+}
+
