8f6e19cda483082b6192b57ee027a68d729445d4
eleventy7 <534336+eleventy7@users.noreply.github.com>
2020-06-14 09:01:06 -0400

    [Cluster] Async Sample

---
+++ cluster-core/src/main/java/com/aeroncookbook/cluster/async/AsyncSample.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019-2020 Shaun Laurens.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aeroncookbook.cluster.async;
+
+import io.aeron.Aeron;
+import io.aeron.Publication;
+import io.aeron.Subscription;
+import io.aeron.driver.MediaDriver;
+import org.agrona.concurrent.AgentRunner;
+import org.agrona.concurrent.IdleStrategy;
+import org.agrona.concurrent.ShutdownSignalBarrier;
+import org.agrona.concurrent.SleepingMillisIdleStrategy;
+
+public class AsyncSample
+{
+    public static void main(String[] args)
+    {
+        final String channel = "aeron:ipc";
+        final int streamToTimer = 10;
+        final int streamToTimerClient = 11;
+        final IdleStrategy idleStrategySend = new SleepingMillisIdleStrategy(7);
+        final IdleStrategy idleStrategyReceive = new SleepingMillisIdleStrategy(5);
+        final ShutdownSignalBarrier barrier = new ShutdownSignalBarrier();
+
+        //construct Media Driver, cleaning up media driver folder on start/stop
+        final MediaDriver.Context mediaDriverCtx = new MediaDriver.Context()
+                .dirDeleteOnStart(true)
+                .dirDeleteOnShutdown(true);
+        final MediaDriver mediaDriver = MediaDriver.launchEmbedded(mediaDriverCtx);
+
+        //construct Aeron, pointing at the media driver's folder
+        final Aeron.Context aeronCtx = new Aeron.Context()
+                .aeronDirectoryName(mediaDriver.aeronDirectoryName());
+        final Aeron aeron = Aeron.connect(aeronCtx);
+
+        //construct the subs and pubs
+        final Subscription timerSubscription = aeron.addSubscription(channel, streamToTimer);
+        final Publication timerPublication = aeron.addPublication(channel, streamToTimer);
+        final Subscription timerClientSubscription = aeron.addSubscription(channel, streamToTimerClient);
+        final Publication timerClientPublication = aeron.addPublication(channel, streamToTimerClient);
+
+        //construct the agents
+        final TimerClientAgent timerClientAgent =
+            new TimerClientAgent(timerPublication, timerClientSubscription);
+        final TimerAgent timerAgent = new TimerAgent(timerSubscription, timerClientPublication, barrier);
+
+        //construct agent runners
+        final AgentRunner timerClientAgentRunner = new AgentRunner(idleStrategySend,
+                Throwable::printStackTrace, null, timerClientAgent);
+        final AgentRunner timerAgentRunner = new AgentRunner(idleStrategyReceive,
+                Throwable::printStackTrace, null, timerAgent);
+
+        //start the runners
+        AgentRunner.startOnThread(timerClientAgentRunner);
+        AgentRunner.startOnThread(timerAgentRunner);
+
+        //wait for the final item to be received before closing
+        barrier.await();
+
+        //close the resources
+        timerAgentRunner.close();
+        timerClientAgentRunner.close();
+        aeron.close();
+        mediaDriver.close();
+    }
+}
---
+++ cluster-core/src/main/java/com/aeroncookbook/cluster/async/TimerClientAgent.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2019-2020 Shaun Laurens.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aeroncookbook.cluster.async;
+
+import com.aeroncookbook.cluster.async.gen.CancelTimerCommand;
+import com.aeroncookbook.cluster.async.gen.ExitCommand;
+import com.aeroncookbook.cluster.async.gen.NewTimerCommand;
+import com.aeroncookbook.cluster.async.gen.TimerCanceledEvent;
+import com.aeroncookbook.cluster.async.gen.TimerFiredEvent;
+import io.aeron.Publication;
+import io.aeron.Subscription;
+import io.aeron.logbuffer.Header;
+import org.agrona.DirectBuffer;
+import org.agrona.ExpandableDirectByteBuffer;
+import org.agrona.concurrent.Agent;
+import org.agrona.concurrent.EpochClock;
+import org.agrona.concurrent.IdleStrategy;
+import org.agrona.concurrent.SleepingMillisIdleStrategy;
+import org.agrona.concurrent.SystemEpochClock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.eider.util.EiderHelper.getEiderId;
+
+public class TimerClientAgent implements Agent
+{
+    private static final int RETRY_COUNT = 3;
+    private static final long TWO_SECONDS = 2_000;
+    private static final long TEN_MILLISECONDS = 10;
+    private static final long EXIT_CORRELATION = 1000;
+    private static final long NORMAL_CORRELATION_START = 5000;
+    private static final long RACE_CORRELATION_START = 10000;
+
+    private final Publication publication;
+    private final Subscription timerClientSubscription;
+    private final EpochClock epochClock = new SystemEpochClock();
+    private final Logger logger = LoggerFactory.getLogger(TimerClientAgent.class);
+
+    private final ExpandableDirectByteBuffer requestBuffer;
+
+    private final IdleStrategy internalIdle = new SleepingMillisIdleStrategy(1);
+
+    private final NewTimerCommand newTimer = new NewTimerCommand();
+    private final CancelTimerCommand cancelTimer = new CancelTimerCommand();
+    private final ExitCommand exit = new ExitCommand();
+    private final TimerCanceledEvent timerCanceledEvent = new TimerCanceledEvent();
+    private final TimerFiredEvent timerFiredEvent = new TimerFiredEvent();
+
+    private State agentState;
+    private long normalCorrelation = NORMAL_CORRELATION_START;
+    private long raceCorrelation = RACE_CORRELATION_START;
+
+    public TimerClientAgent(final Publication publication,
+                            final Subscription timerClientSubscription)
+    {
+        this.publication = publication;
+        this.timerClientSubscription = timerClientSubscription;
+        this.agentState = State.INIT;
+        this.requestBuffer =
+            new ExpandableDirectByteBuffer(Math.max(NewTimerCommand.BUFFER_LENGTH, CancelTimerCommand.BUFFER_LENGTH));
+    }
+
+    @Override
+    public int doWork()
+    {
+        final long nowMs = epochClock.time();
+
+        switch (agentState)
+        {
+            case INIT:
+                scheduleExit();
+                agentState = State.NORMAL;
+                break;
+            case NORMAL:
+                scheduleTimer(normalCorrelation, nowMs + TEN_MILLISECONDS);
+                removeTimer(normalCorrelation);
+                normalCorrelation += 1;
+                break;
+            case RACE:
+                scheduleTimer(raceCorrelation, nowMs);
+                removeTimer(raceCorrelation);
+                raceCorrelation += 1;
+                break;
+            default:
+                break;
+        }
+
+        timerClientSubscription.poll(this::handler, 1);
+
+        return 0;
+    }
+
+    private void handler(DirectBuffer buffer, int offset, int length, Header header)
+    {
+        short eiderId = getEiderId(buffer, offset);
+        switch (eiderId)
+        {
+            case TimerFiredEvent.EIDER_ID:
+                timerFiredEvent.setUnderlyingBuffer(buffer, offset);
+                acceptTimerFired(timerFiredEvent);
+                break;
+            case TimerCanceledEvent.EIDER_ID:
+                timerCanceledEvent.setUnderlyingBuffer(buffer, offset);
+                acceptTimerCanceled(timerCanceledEvent);
+                break;
+            default:
+                internalIdle.idle();
+                break;
+        }
+    }
+
+    private void scheduleExit()
+    {
+        logger.info("Scheduling exit with correlation {}", EXIT_CORRELATION);
+        scheduleTimer(EXIT_CORRELATION, epochClock.time() + TWO_SECONDS);
+    }
+
+    private void scheduleTimer(long correlation, long deadlineMs)
+    {
+        logger.info("Scheduling timer with correlation {} for deadline {}", correlation, deadlineMs);
+        awaitConnected();
+
+        newTimer.setUnderlyingBuffer(requestBuffer, 0);
+        newTimer.writeHeader();
+        newTimer.writeCorrelation(correlation);
+        newTimer.writeDeadline(deadlineMs);
+
+        int attempts = RETRY_COUNT;
+        do
+        {
+            final long result = publication.offer(requestBuffer, 0, NewTimerCommand.BUFFER_LENGTH);
+            if (result > 0)
+            {
+                break;
+            }
+        }
+        while (--attempts > 0);
+    }
+
+    private void removeTimer(long correlation)
+    {
+        logger.info("Canceling timer with correlation {}", correlation);
+        awaitConnected();
+
+        cancelTimer.setUnderlyingBuffer(requestBuffer, 0);
+        cancelTimer.writeHeader();
+        cancelTimer.writeCorrelation(correlation);
+
+        int attempts = RETRY_COUNT;
+        do
+        {
+            final long result = publication.offer(requestBuffer, 0, CancelTimerCommand.BUFFER_LENGTH);
+            if (result > 0)
+            {
+                break;
+            }
+        }
+        while (--attempts > 0);
+    }
+
+    private void awaitConnected()
+    {
+        while (!publication.isConnected())
+        {
+            internalIdle.idle();
+        }
+    }
+
+    private void acceptTimerFired(TimerFiredEvent timerFiredEvent)
+    {
+        logger.info("Timer Fired Event! with correlation {}", timerFiredEvent.readCorrelation());
+        if (timerFiredEvent.readCorrelation() == EXIT_CORRELATION)
+        {
+            sendExit();
+        } else if (timerFiredEvent.readCorrelation() >= RACE_CORRELATION_START)
+        {
+            this.agentState = State.AWAITING_EXIT;
+        }
+    }
+
+    private void sendExit()
+    {
+        logger.info("Sending exit command to Timer");
+        awaitConnected();
+
+        exit.setUnderlyingBuffer(requestBuffer, 0);
+        exit.writeHeader();
+
+        int attempts = RETRY_COUNT;
+        do
+        {
+            final long result = publication.offer(requestBuffer, 0, ExitCommand.BUFFER_LENGTH);
+            if (result > 0)
+            {
+                break;
+            }
+        }
+        while (--attempts > 0);
+    }
+
+    private void acceptTimerCanceled(TimerCanceledEvent timerCanceledEvent)
+    {
+        logger.info("Timer Canceled Event! with correlation {}", timerCanceledEvent.readCorrelation());
+        if (timerCanceledEvent.readCorrelation() >= NORMAL_CORRELATION_START)
+        {
+            this.agentState = State.RACE;
+        }
+    }
+
+    @Override
+    public String roleName()
+    {
+        return "timer-client";
+    }
+
+    enum State
+    {
+        INIT,
+        NORMAL,
+        RACE,
+        AWAITING_EXIT
+    }
+
+}
