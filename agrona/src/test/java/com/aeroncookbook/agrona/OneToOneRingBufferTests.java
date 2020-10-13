23f2b0024894a78db10918df4155b9dd93a48d37
eleventy7 <534336+eleventy7@users.noreply.github.com>
2020-10-13 17:41:09 -0400

    Add OneToOneRingBuffer example

---
+++ agrona/src/test/java/com/aeroncookbook/agrona/OneToOneRingBufferTests.java
@@ -0,0 +1,58 @@
+package com.aeroncookbook.agrona;
+
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.MessageHandler;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.agrona.concurrent.ringbuffer.OneToOneRingBuffer;
+import org.agrona.concurrent.ringbuffer.RingBufferDescriptor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.util.HashSet;
+
+class OneToOneRingBufferTests
+{
+
+    private final String testString = "0123456789";
+
+    @Test
+    void canSendAndReceive()
+    {
+        final int bufferLength = 4096 + RingBufferDescriptor.TRAILER_LENGTH;
+        final UnsafeBuffer internalBuffer = new UnsafeBuffer(ByteBuffer.allocateDirect(bufferLength));
+        final OneToOneRingBuffer ringBuffer = new OneToOneRingBuffer(internalBuffer);
+        final MessageCapture capture = new MessageCapture();
+
+        final UnsafeBuffer toSend = new UnsafeBuffer(ByteBuffer.allocateDirect(10));
+        toSend.putStringWithoutLengthAscii(0, testString);
+
+        for (int i = 0; i < 10000; i++)
+        {
+            for (int k = 0; k < 20; k++)
+            {
+                ringBuffer.write(1, toSend, 0, 10);
+            }
+            ringBuffer.read(capture, 40);
+        }
+
+        Assertions.assertEquals(1, capture.receivedStrings.size());
+        Assertions.assertTrue(capture.receivedStrings.contains(testString));
+        Assertions.assertEquals(200000, capture.count);
+    }
+
+    class MessageCapture implements MessageHandler
+    {
+
+        private HashSet<String> receivedStrings = new HashSet<>();
+        private int count = 0;
+
+        @Override
+        public void onMessage(int msgTypeId, MutableDirectBuffer buffer, int index, int length)
+        {
+            receivedStrings.add(buffer.getStringWithoutLengthAscii(index, length));
+            count++;
+        }
+
+    }
+}
