484d764e8a4363a8e95e44a5b1f43ca2e605033b
Shaun L <534336+eleventy7@users.noreply.github.com>
2023-03-09 19:22:50 -0500

    Remove aeroncookbook docker image; build docker from Azul image

---
+++ archive-replication/archive-host/setup-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "debconf debconf/frontend select noninteractive" | debconf-set-selections
+
+apt-get update --quiet
+
+apt-get dist-upgrade --quiet --assume-yes
+
+apt-get install \
+    --quiet \
+    --assume-yes \
+    --no-install-recommends \
+    bash \
+    wget \
+    iproute2 \
+    less \
+    dnsutils \
+    procps \
+    sysstat
+
+mkdir /root/aeron
+mkdir /root/jar
+
+wget https://repo1.maven.org/maven2/io/aeron/aeron-all/1.40.0/aeron-all-1.40.0.jar -P /root/aeron/
+wget https://repo1.maven.org/maven2/io/aeron/aeron-agent/1.40.0/aeron-agent-1.40.0.jar -P /root/aeron/
+
+apt-get remove wget --quiet --assume-yes
