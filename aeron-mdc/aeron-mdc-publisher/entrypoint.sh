a49b7ec518749d4a768b98cba9b1019fd7c2756e
eleventy7 <534336+eleventy7@users.noreply.github.com>
2023-04-14 10:22:59 -0400

    Update to latest Aeron, Agrona, SBE

--- aeron-mdc/aeron-mdc-publisher/entrypoint.sh
+++ aeron-mdc/aeron-mdc-publisher/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 java --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
-  -javaagent:/root/aeron/aeron-agent-1.40.0.jar \
+  -javaagent:/root/aeron/aeron-agent-1.41.0.jar \
   -Djava.net.preferIPv4Stack=true \
   -Daeron.event.log=admin \
   -jar /root/jar/aeron-mdc-publisher-uber.jar
