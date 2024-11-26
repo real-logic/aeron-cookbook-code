61af0ac072eb7c6e09339b447d0a61b88e48b58e
Nate Bradac <nate.bradac@weareadaptive.com>
2024-11-25 23:06:18 -0600

    add dependencyUpdates task, update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     java
     checkstyle
+    alias(libs.plugins.versions)
 }
 
 java {
@@ -32,5 +35,19 @@ allprojects {
         mavenLocal()
         mavenCentral()
         google()
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    // Reject all non stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
